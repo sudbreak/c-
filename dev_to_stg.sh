6f0ef78d1ea53bc7c19f34a5064291a697389e68
sudbreak <lana@smdh.ru>
2025-05-09 20:35:34 +0300

    Add deployment scripts

--- dev_to_stg.sh
+++ dev_to_stg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git checkout stg
-git merge dev -m "Merge from dev $(date +'%Y-%m-%d %H:%M:%S')"
-git tag -a "stg-$(date +'%Y%m%d%H%M%S')" -m "Release to staging"
+git merge dev -m "Merge from dev $(date +%Y-%m-%d)"
+git tag -a "stg-$(date +%Y%m%d)" -m "Staging release"
 git push origin stg --tags
 git checkout dev
--- stg_to_prd.sh
+++ stg_to_prd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git checkout prd
-git merge stg -m "Merge from stg $(date +'%Y-%m-%d %H:%M:%S')"
-git tag -a "prd-$(date +'%Y%m%d%H%M%S')" -m "Release to production"
+git merge stg -m "Merge from stg $(date +%Y-%m-%d)"
+git tag -a "prd-$(date +%Y%m%d)" -m "Production release"
 git push origin prd --tags
 git checkout dev
